cc1fad10b00d7f467299f8acd0c7f61882b4dd45
gulhayatguder <gulhayatguder31@gmail.com>
2023-01-02 12:26:24 +0100

    02.01

---
+++ src/day26_inheritance/ECorolla.java
@@ -0,0 +1,11 @@
+package day26_inheritance;
+
+
+    public class ECorolla extends DToyota{
+
+        String model= "Corolla";
+        String uretimYeri="Turkiye";
+        String teker="200.55 teker";
+        String vites="vites belirtilmedi";
+    }
+
